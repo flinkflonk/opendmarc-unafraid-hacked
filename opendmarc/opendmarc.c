2e000574bdf2c7ce4507c902fd0ca778b32e0173
Michael Hinz <michael@hinz.no>
2016-10-19 12:02:15 +0200

    Bug #194: wrong DMARC state in Auth-Res header, patch by Juri Haberland

--- opendmarc/opendmarc.c
+++ opendmarc/opendmarc.c
@@ -2995,8 +2995,13 @@ mlfi_eom(SMFICTX *ctx)
 	{
 	  case DMARC_POLICY_ABSENT:		/* No DMARC record found */
 	  case DMARC_FROM_DOMAIN_ABSENT:	/* No From: domain */
-	  case DMARC_POLICY_NONE:		/* Accept and report */
 		aresult = "none";
+		ret = SMFIS_ACCEPT;
+		result = DMARC_RESULT_ACCEPT;
+		break;
+
+	  case DMARC_POLICY_NONE:		/* Alignment failed, but policy is none: */
+		aresult = "fail";
 		ret = SMFIS_ACCEPT;
 		result = DMARC_RESULT_ACCEPT;
 		break;
